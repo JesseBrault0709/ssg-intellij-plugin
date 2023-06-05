029342f4694bb357c1a395022eba807823c8d063
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2023-06-05 10:39:46 +0200

    Using newest gst-intellij-plugin code.

--- src/main/java/com/jessebrault/ssg/intellij/lang/parser/SsgParser.java
+++ src/main/java/com/jessebrault/ssg/intellij/lang/parser/SsgParser.java
@@ -4,16 +4,14 @@ import com.intellij.lang.ASTNode;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiParser;
 import com.intellij.psi.tree.IElementType;
-import com.jessebrault.gst.intellij.parser.GstParser;
+import com.jessebrault.gst.intellij.parser.EGstParser;
 import org.jetbrains.annotations.NotNull;
 
 public final class SsgParser implements PsiParser {
 
-    private final PsiParser delegate = new GstParser();
-
     @Override
     public @NotNull ASTNode parse(@NotNull IElementType root, @NotNull PsiBuilder builder) {
-        return this.delegate.parse(root, builder);
+        return new EGstParser().parse(root, builder);
     }
 
 }
